cef7c0a6516cfcc5c86cae2054f9a356f1af8d0a
Caleb Russel <calebrussel77@gmail.com>
2023-10-13 17:03:25 +0200

    fix: update socket io implementations

--- src/pages/api/socket/io.ts
+++ src/pages/api/socket/io.ts
@@ -9,13 +9,58 @@ export const config = {
   },
 };
 
+// function createSocketServer(server: never) {
+//   const io = new Server(server, {
+//     path: '/api/socket/io',
+//     addTrailingSlash: false,
+//   });
+
+//   io.on('connection', socket => {
+//     console.log(`[SOCKET-SERVER]: user connected: ${socket.id}`);
+
+//     socket.on('disconnect', () => {
+//       console.log(`[SOCKET-SERVER]: user disconnected: ${socket.id}`);
+//     });
+
+//     socket.on('error', error => {
+//       console.log(`[SOCKET-SERVER]: socket error : ${error?.message}`);
+//     });
+//   });
+
+//   return io;
+// }
+
+const onError = (error: Error) => {
+  console.log({ error });
+  console.log(`[SERVER]: server error : ${error?.message}`);
+};
+
+const onListenning = () => {
+  console.log(`[SERVER]: server listening...`);
+};
+
 const SocketHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
+  res?.socket?.server?.on('error', onError);
+  res?.socket?.server?.on('listening', onListenning);
+
   if (!res?.socket?.server?.io) {
-    console.log('Socket is initializing');
     const io = new Server(res.socket.server as never, {
       path: '/api/socket/io',
       addTrailingSlash: false,
     });
+
+    io.on('connection', socket => {
+      console.log(`[SOCKET-SERVER]: user connected: ${socket.id}`);
+
+      socket.on('disconnect', () => {
+        console.log(`[SOCKET-SERVER]: user disconnected: ${socket.id}`);
+      });
+
+      socket.on('error', error => {
+        console.log(`[SOCKET-SERVER]: socket error : ${error?.message}`);
+      });
+    });
+
     res.socket.server.io = io;
   }
   res.end();
